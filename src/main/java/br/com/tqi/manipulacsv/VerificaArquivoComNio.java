c97e70db3dfabd576648be911798b92e1c1d2ea8
Marlon Lopes Pinto <marlon.pinto@tqi.com.br>
2018-04-27 18:29:05 -0300

    classe test validar titulo

--- src/main/java/br/com/tqi/manipulacsv/VerificaArquivoComNio.java
+++ src/main/java/br/com/tqi/manipulacsv/VerificaArquivoComNio.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.function.Function;
 
 public class VerificaArquivoComNio {
 
@@ -12,14 +11,29 @@ public class VerificaArquivoComNio {
 		Path caminho = Paths.get("teste.csv");
 		
 		boolean possuiIdadeAbaixoDe30 = Files.lines(caminho)
-			.map(s -> s.replaceAll("\\D", "")) // parser
-			.map(i -> Integer.valueOf(i)) // conversao
+			.map(s -> s.replaceAll("\\D", "")) // parser (converte o que não for número em "")
+			.map(i -> Integer.valueOf(i)) // conversao (converte o que sobrou em inteiro)
 			.filter(i -> i < 30) // aplica a logica
 			.findAny().isPresent(); // procura se encontrou alguem
 		 
 		System.out.println(possuiIdadeAbaixoDe30);
 		
 		return possuiIdadeAbaixoDe30;
+		
+	}
+	
+	public static boolean validarTituloIgualDefinido() throws IOException {
+		Path caminho = Paths.get("teste.csv");
+		
+		boolean tituloEstaIgual = Files.lines(caminho)
+			.map(s -> s.replaceAll("\\D", "")) // parser (converte o que não for número em "")
+			.map(i -> Integer.valueOf(i)) // conversao (converte o que sobrou em inteiro)
+			.filter(i -> i < 30) // aplica a logica
+			.findAny().isPresent(); // procura se encontrou alguem
+		 
+		System.out.println(tituloEstaIgual);
+		
+		return tituloEstaIgual;
 		
 	}
 
